4082ca04007b64ab7f6117c3e3efa396ecdef5f8
Windawz <46968683+Windawz@users.noreply.github.com>
2021-06-10 18:53:33 +0300

    Write code for counting letters; move input functions to the Game class; merge all code into one file

--- CSharp/CSharpPilot1/Program.cs
+++ CSharp/CSharpPilot1/Program.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpPilot1 {
     class Program {
         static void Main(string[] args) {
+        }
+    }
+    static class Game {
+        public static Dictionary<char, int> GetLetters(string word) {
+            switch (word.Length) {
+                case 0:
+                    return new Dictionary<char, int>();
+
+                default:
+                    string s = word.Substring(0, word.Length - 1);
+                    char c = word[word.Length - 1];
+                    var d = new Dictionary<char, int>(GetLetters(s));
+                    d[c] = d.ContainsKey(c) ? d[c] + 1 : 1;
+                    return d;
+            }
+        }
+        public static string? GetWord() {
+            string word = GetInput();
+            if (IsValidWord(word)) {
+                return word;
+            } else {
+                return null;
+            }
+        }
+        private static string GetInput() {
+            return Console.ReadLine().Trim();
+        }
+        private static bool IsValidWord(string word) {
+            return
+                !string.IsNullOrEmpty(word) &&
+                !word.Contains(' ') &&
+                (word.Length >= 8 && word.Length <= 30);
         }
     }
 }
