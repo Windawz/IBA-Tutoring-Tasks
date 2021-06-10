9f3df932435dcc3500a6d1712f96ca505069b1ea
Windawz <46968683+Windawz@users.noreply.github.com>
2021-06-10 16:43:37 +0300

    Write some basic input functions

---
+++ CSharp/CSharpPilot1/Input.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharpPilot1 {
+    static class Input {
+        private static string GetInput() {
+            return Console.ReadLine().Trim();
+        }
+        private static bool IsValidWord(string word) {
+            return 
+                !string.IsNullOrEmpty(word) && 
+                !word.Contains(' ') &&
+                (word.Length >= 8 && word.Length <= 30);
+        }
+        public static string? GetWord() {
+            string word = GetInput();
+            if (IsValidWord(word)) {
+                return word;
+            } else {
+                return null;
+            }
+        }
+    }
+}
