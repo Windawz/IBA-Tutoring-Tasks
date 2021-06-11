6f8a8c8f82888421bdf572b4cb78e056902c889e
Windawz <46968683+Windawz@users.noreply.github.com>
2021-06-11 14:24:41 +0300

    Work on game code

--- CSharp/CSharpPilot1/Program.cs
+++ CSharp/CSharpPilot1/Program.cs
@@ -4,9 +4,22 @@ using System.Collections.Generic;
 namespace CSharpPilot1 {
     class Program {
         static void Main(string[] args) {
+            
         }
     }
     static class Game {
+        public static string RequestPlayerInput(string message, string errorMessage) {
+            while (true) {
+                Println("{0}", message);
+                Print(">");
+                string? result = GetWord();
+                if (result != null) {
+                    return result;
+                } else {
+                    Println("{0}", errorMessage);
+                }
+            }
+        }
         public static Dictionary<char, int> GetLetters(string word) {
             var d = new Dictionary<char, int>();
             foreach (char c in word) {
@@ -14,7 +27,13 @@ namespace CSharpPilot1 {
             }
             return d;
         }
-        public static string? GetWord() {
+        public static void Print(string format, params object[] arg) =>
+            Console.Write(format, arg);
+        public static void Println(string format, params object[] arg) {
+            Print(format, arg);
+            Console.WriteLine();
+        }
+        private static string? GetWord() {
             string word = GetInput();
             if (IsValidWord(word)) {
                 return word;
@@ -22,14 +41,11 @@ namespace CSharpPilot1 {
                 return null;
             }
         }
-        private static string GetInput() {
-            return Console.ReadLine().Trim();
-        }
-        private static bool IsValidWord(string word) {
-            return
-                !string.IsNullOrEmpty(word) &&
-                !word.Contains(' ') &&
-                (word.Length >= 8 && word.Length <= 30);
-        }
+        private static string GetInput() =>
+            Console.ReadLine().Trim();
+        private static bool IsValidWord(string word) =>
+            !string.IsNullOrEmpty(word) &&
+            !word.Contains(' ') &&
+            (word.Length >= 8 && word.Length <= 30);
     }
 }
