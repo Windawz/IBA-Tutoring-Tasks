d22012adc03d1e42b979fea06b8e89ac80d25121
Windawz <46968683+Windawz@users.noreply.github.com>
2021-06-11 12:44:23 +0300

    Simplify GetLetters()

--- CSharp/CSharpPilot1/Program.cs
+++ CSharp/CSharpPilot1/Program.cs
@@ -8,17 +8,11 @@ namespace CSharpPilot1 {
     }
     static class Game {
         public static Dictionary<char, int> GetLetters(string word) {
-            switch (word.Length) {
-                case 0:
-                    return new Dictionary<char, int>();
-
-                default:
-                    string s = word.Substring(0, word.Length - 1);
-                    char c = word[word.Length - 1];
-                    var d = new Dictionary<char, int>(GetLetters(s));
-                    d[c] = d.ContainsKey(c) ? d[c] + 1 : 1;
-                    return d;
+            var d = new Dictionary<char, int>();
+            foreach (char c in word) {
+                d[c] = d.ContainsKey(c) ? d[c] + 1 : 1;
             }
+            return d;
         }
         public static string? GetWord() {
             string word = GetInput();
