91da1bacd1c9344504d1e952443556a80dc052e9
Windawz <46968683+Windawz@users.noreply.github.com>
2021-06-12 14:59:33 +0300

    Rewrite code in a functional manner, according to the task

--- CSharp/CSharpPilot1/Program.cs
+++ CSharp/CSharpPilot1/Program.cs
@@ -1,49 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Text;
 
 namespace CSharpPilot1 {
     class Program {
-        static void Main(string[] args) {
+        const int MinWordLength = 8;
+        const int MaxWordLength = 30;
+        const int MaxPlayers = 2;
+        const double MaxSeconds = 10.0;
 
-        }
-        static string RequestPlayerInput(string message, string errorMessage) {
-            while (true) {
-                Println("{0}", message);
-                Print(">");
-                string? result = GetWord();
-                if (result != null) {
-                    return result;
-                } else {
-                    Println("{0}", errorMessage);
-                }
+        class GameState {
+            public GameState(int player, Input input) {
+                Player = player;
+                Input = input;
             }
+            public GameState(GameState other) : this(other.Player, other.Input) { }
+
+
+            public int Player { get; }
+            public Input Input { get; }
         }
-        static Dictionary<char, int> GetLetters(string word) {
-            var d = new Dictionary<char, int>();
-            foreach (char c in word) {
-                d[c] = d.ContainsKey(c) ? d[c] + 1 : 1;
+        class Input {
+            public Input(string word, double seconds) {
+                Word = word;
+                Seconds = seconds;
             }
-            return d;
+
+            public string Word { get; }
+            public double Seconds { get; }
         }
-        static void Println(string format, params object[] arg) {
-            Print(format, arg);
-            Console.WriteLine();
-        }
-        static void Print(string format, params object[] arg) =>
-            Console.Write(format, arg);
-        static string? GetWord() {
-            string word = GetInput();
-            if (IsValidWord(word)) {
-                return word;
-            } else {
-                return null;
+        class Output {
+            public Output() {
+                sb = new StringBuilder();
             }
+            public Output(string format, params object[] arg) : this() {
+                sb.Append(string.Format(format, arg));
+            }
+            public Output(string str) : this() {
+                sb.Append(str);
+            }
+
+            private StringBuilder sb;
+
+            public static void Print(Output output) =>
+                Console.Write(output.ToString());
+            public Output Add(string format, params object[] arg) =>
+                new Output(this + string.Format(format, arg));
+            public Output AddLine() =>
+                new Output(this + Environment.NewLine);
+            public Output AddLine(string format, params object[] arg) =>
+                Add(format, arg).AddLine();
+            public override string ToString() => sb.ToString();
         }
-        static string GetInput() =>
-            Console.ReadLine().Trim();
-        private static bool IsValidWord(string word) =>
-            !string.IsNullOrEmpty(word) &&
-            !word.Contains(' ') &&
-            (word.Length >= 8 && word.Length <= 30);
+
+        static void Main(string[] args) {
+            GameState endState = Play(null);
+        }
+        static GameState Play(GameState? lastState) {
+            Func<GameState, bool> isPlayerDefeated =
+                state => (state.Input.Seconds > MaxSeconds) || !HasSameLetters(GetInput().Word, state.Input.Word);
+            Func<Input, bool> isInputValid =
+                input => !string.IsNullOrEmpty(input.Word) &&
+                    !input.Word.Contains(' ') &&
+                    (input.Word.Length >= MinWordLength && input.Word.Length <= MaxWordLength);
+
+            return lastState switch {
+                null => Play(new GameState(0, GetInput())),
+                _ when !isInputValid(lastState.Input) => Play(new GameState(lastState)),
+                _ when isPlayerDefeated(lastState) => new GameState(lastState),
+                _ => Play(new GameState((lastState.Player + 1) % MaxPlayers, GetInput())),
+            };
+        }
+
+        static bool HasSameLetters(string word, string lastWord) =>
+            word.Length == lastWord.Length && GetLetterCounts(word).Intersect(GetLetterCounts(lastWord)).Any();
+        static IEnumerable<KeyValuePair<char, int>> GetLetterCounts(string word) =>
+            word.Select(x => new KeyValuePair<char, int>(x, word.Count(y => y == x)));
+
+        // Wrap because the inner function is impure.
+        static Input GetInput() {
+            double seconds = 0.0;
+
+            var timer = new Timer(100);
+            timer.Elapsed += (sender, e) => seconds += 0.1;
+
+            timer.Start();
+            string word = Console.ReadLine();
+            timer.Stop();
+
+            return new Input(word, seconds);
+        }
+
     }
 }
