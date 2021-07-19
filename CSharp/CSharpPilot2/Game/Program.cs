d3568b625604851a3e3cdfa86111cb543423ba7d
Windawz <46968683+Windawz@users.noreply.github.com>
2021-07-19 04:03:01 +0300

    Remove the usage of the now removed IO class from main program code

--- CSharp/CSharpPilot2/Game/Program.cs
+++ CSharp/CSharpPilot2/Game/Program.cs
@@ -1,55 +1,71 @@
 ﻿using System;
 using System.Linq;
+using System.Timers;
 
 namespace Game {
-
     class Program {
         static void Main(string[] args) {
-            _ = IO.WriteLine(GetIntroductionString());
-            _ = IO.ReadKey(intercept: true);
-            IO<State> result = Loop(new State());
+            Console.WriteLine(GetIntroductionString());
+            Console.ReadKey(intercept: true);
+            State finalState = Play(new State());
         }
 
-        // The main game loop.
-        // Located in a separate function to allow recursion - the only truly functional way of looping something.
-        static IO<State> Loop(State state) => state switch {
-            _ when state.Over => IO.WriteLine(GetDefeatedString(state)).Bind(_ =>
-                state.AsIO()),
-            _ when state.Last is null =>
-                new Player(0, false).AsIO().Bind(p =>
-                GetInputInfo(p.Index, 0.0, GetInputRequestString(p.Index)).Bind(inputInfo =>
-                Loop(state.AddStep(new Step(p, inputInfo)))
-                )),
-            _ =>
-                new Step(state.Last).AsIO().Bind(last =>
-                GetInputInfo(
-                    Rules.NextPlayer(last.Player.Index),
-                    0.0,
-                    GetInputRequestString(
-                        Rules.NextPlayer(last.Player.Index)
-                    )
-                ).Bind(inputInfo =>
-                new Player(
-                    Rules.NextPlayer(last.Player.Index),
-                    !Rules.IsInputCompetentText(inputInfo, last.InputInfo) || !Rules.IsInputCompetentTime(inputInfo)
-                ).AsIO().Bind(p =>
-                Loop(state.AddStep(new Step(p, inputInfo)))
-                ))),
-        };
+        // Main game logic.
+        // Recursive.
+        // Returns the final state of the game once it has played out to the end.
+        static State Play(State state) {
+            if (state.Over) {
+                Console.WriteLine(GetDefeatedString(state));
+                return state;
+            } else if (state.Last is null) {
+                var newPlayer = new Player(0, false);
+                var inputInfo = GetInputInfo(newPlayer.Index, 0.0, GetInputRequestString(newPlayer.Index));
+                var newStep = new Step(newPlayer, inputInfo);
+                var newState = state.AddStep(newStep);
+                return Play(newState);
+            } else {
+                int newPlayerIndex = Rules.NextPlayer(state.Last.Player.Index);
+                var inputInfo = GetInputInfo(newPlayerIndex, 0.0, GetInputRequestString(newPlayerIndex));
+                bool defeated =
+                    !Rules.IsInputCompetentText(inputInfo, state.Last.InputInfo) ||
+                    !Rules.IsInputCompetentTime(inputInfo);
+                var newPlayer = new Player(newPlayerIndex, defeated);
+                var newStep = new Step(newPlayer, inputInfo);
+                var newState = state.AddStep(newStep);
+                return Play(newState);
+            }
+        }
 
         // Keeps on trying to get valid input from player.
         // Will execute over and over on failure.
         // Each attempt the time spent on input will carry over until the input passes.
         // 'startTime': how much time has been already taken by the player to enter input.
         // 'requestString': message to display to the player before requesting input.
-        static IO<InputInfo> GetInputInfo(int playerIndex, double startTime, string requestString) =>
-            IO.WriteLine($"{requestString} {GetTimeLeftString(Rules.MaxSeconds - startTime)}").Bind(_ =>
-            IO.ReadLineValidated(Rules.IsInputTextValid).Bind(inputInfo =>
-            inputInfo switch {
-                _ when !inputInfo.Valid => GetInputInfo(playerIndex, inputInfo.Time + startTime, GetInputRetryString()),
-                _ => new InputInfo(inputInfo.Text, inputInfo.Time + startTime, inputInfo.Valid).AsIO(),
+        static InputInfo GetInputInfo(int playerIndex, double startTime, string requestString) {
+            Console.WriteLine($"{requestString} {GetTimeLeftString(Rules.MaxSeconds - startTime)}");
+            var inputInfo = ReadLineValidated(Rules.IsInputTextValid);
+            double accumulatedTime = inputInfo.Time + startTime;
+            if (!inputInfo.Valid) {
+                return GetInputInfo(playerIndex, inputInfo.Time + startTime, GetInputRetryString());
+            } else {
+                return new InputInfo(inputInfo.Text, inputInfo.Time + startTime, inputInfo.Valid);
             }
-            ));
+        }
+        // Reads user input, just like Console.ReadLine().
+        // The input is validated using the passed function.
+        // The validation result will be returned along with the rest of the input.
+        static InputInfo ReadLineValidated(Func<string, bool> textValidator) {
+            double time = 0.0;
+
+            var timer = new Timer(100);
+            timer.Elapsed += (sender, e) => time += 0.1;
+
+            timer.Start();
+            string text = Console.ReadLine() ?? "";
+            timer.Stop();
+
+            return new InputInfo(text, time, textValidator(text));
+        }
         static string GetInputRequestString(int playerIndex) =>
             $"Игрок {playerIndex + 1}, введите слово:";
         static string GetInputRetryString() =>
