35dab4113f142cbf7dfb1a0d058caeae1950d681
Windawz <46968683+Windawz@users.noreply.github.com>
2021-07-23 18:58:53 +0300

    State: simplify, make History a List

--- CSharp/CSharpPilot2/Game/State.cs
+++ CSharp/CSharpPilot2/Game/State.cs
@@ -1,16 +1,11 @@
 ﻿using System.Linq;
-using System.Collections.Immutable;
+using System.Collections.Generic;
 
 namespace Game {
     class State {
-        public State(ImmutableList<Step> history) => History = history;
-        public State() : this(ImmutableList.Create<Step>()) { }
+        public State() { }
 
-        public ImmutableList<Step> History { get; }
-        public Step? LastStep => History.LastOrDefault();
-        public bool IsOver => LastStep?.Player.IsDefeated ?? false;
-
-        public State AddStep(Step step) =>
-            new State(History.Add(step));
+        public List<Step> History { get; } = new();
+        public bool IsOver => History.LastOrDefault()?.Player.IsDefeated ?? false;
     }
 }
