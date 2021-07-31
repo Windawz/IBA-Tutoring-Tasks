ec34fa34f10220b61f6ecb016e03f4189eed46dc
Windawz <46968683+Windawz@users.noreply.github.com>
2021-07-31 23:57:15 +0300

    Implement EnglishLocale

---
+++ CSharp/CSharpPilot2/CSharpPilot2/Gameplay/EnglishLocale.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPilot2.Gameplay
+{
+    internal class EnglishLocale : Locale
+    {
+        public override string GetNameRequestString(int playerIndex) =>
+            $"Player {playerIndex + 1}, enter name:";
+        public override string GetWordRequestString(string playerName) =>
+            $"\"{playerName}\", enter word:";
+        public override string GetInvalidWordString(string word) =>
+            $"Word \"{word}\" is invalid!";
+        public override string GetInvalidInputString() =>
+            $"Invalid input, try again:";
+        public override string GetTimeLeftSuffixString(double timeLeft) =>
+            $"{FormatTime(timeLeft)} left";
+        public override string GetGameOverLoserString(string loserName) =>
+            $"Player \"{loserName}\" has lost!";
+        public override string GetGameOverPreviousWordString(string word, double seconds) =>
+            $"Previous word: \"{word}\" ({FormatTime(seconds)})";
+        public override string GetGameOverCurrentWordString(string word, double seconds) =>
+            $"Current word: \"{word}\" ({FormatTime(seconds)})";
+        public override string GetPressAnyKeyToContinueString() =>
+            $"Press any key to continue...";
+        public override string GetIntroTitleString() =>
+            $"Welcome to \"Game of \'Words\"!";
+        public override string GetIntroRulesTitleString() =>
+            $"The rules are:";
+        public override string GetIntroRuleFirstString(int playerCount) =>
+            $"{playerCount} player(s), one after another, enter their word consisting of the previous word's letters.";
+        public override string GetIntroRuleSecondString() =>
+            $"The word entered must differ from the previous.";
+        public override string GetIntroRuleThirdString() =>
+            $"The first player receives carte blanche.";
+        public override string GetIntroRuleFourthString(int minWordTextLength, int maxWordTextLength) =>
+            $"If the word's length is less than {minWordTextLength} or greater than {maxWordTextLength} characters, the input must be retried.";
+        public override string GetIntroRuleFifthString(double maxSeconds) =>
+            $"You have {FormatTime(maxSeconds)} for input.";
+        public override string GetIntroRuleSixthString() =>
+            $"On invalid input time isn't restored and keeps going.";
+
+        protected override string FormatTime(double seconds) =>
+            $"{seconds:F}s";
+    }
+}
