e6ac3bbf5bfcc74f6a99caf167919e42e3f8eaa2
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-03 17:59:29 +0300

    Implement ParseParameters for ParameterTemplate

--- CSharp/CSharpPilot2/CSharpPilot2/Commands/ParameterTemplate.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/ParameterTemplate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,40 @@ using System.Threading.Tasks;
 
 namespace CSharpPilot2.Commands
 {
-    internal record ParameterTemplate(string Name, string[] Args);
+    internal record ParameterTemplate(string Name, string[] Args)
+    {
+        static ParameterTemplate[] ParseParameters(string parameters, ParameterParseOptions options)
+        {
+            string[] tokens = parameters.Split(
+                options.ArgDelimiters,
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+            );
+
+            string?                     curParam        = null;
+            List<string>                args            = new();
+            List<ParameterTemplate>     templates       = new();
+
+            foreach (string token in tokens)
+            {
+                if (token.StartsWith(options.Prefix))
+                {
+                    if (args.Count > 0 && curParam is not null)
+                    {
+                        string name = curParam.Substring(1);
+                        ParameterTemplate template = new(name, args.ToArray());
+                        templates.Add(template);
+                        args.Clear();
+                    }
+
+                    curParam = token;
+                }
+                else
+                {
+                    args.Add(token);
+                }
+            }
+
+            return templates.ToArray();
+        }
+    }
 }
