839ec69d2e85c94e169499481cf61accdb68afc2
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-03 19:15:09 +0300

    Start working on Commands.Context

---
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CSharpPilot2.Gameplay;
+
+namespace CSharpPilot2.Commands
+{
+    // I hate to couple it with the Gameplay classes but I don't want to take another long detour.
+    internal record Context(
+        Locale Locale
+    );
+}
