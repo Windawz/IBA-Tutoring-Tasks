6ceeae3f12f828545d9acbe97560c0d752679422
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-03 18:10:49 +0300

    Add error conditions to ParseCommand

--- CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandTemplate.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandTemplate.cs
@@ -8,9 +8,19 @@ namespace CSharpPilot2.Commands
 {
     internal record CommandTemplate(string Name, ParameterTemplate[] Parameters)
     {
+        /// <exception cref="ArgumentException"></exception>
         public static CommandTemplate ParseCommand(string command, CommandParseOptions options)
         {
-            
+            if (String.IsNullOrEmpty(command))
+            {
+                throw new ArgumentException($"Command string is empty", nameof(command));
+            }
+            if (!command.StartsWith(options.Prefix))
+            {
+                throw new ArgumentException($"Command string doesn't start with {options.Prefix}", nameof(command));
+            }
+
+
         }
     }
 }
