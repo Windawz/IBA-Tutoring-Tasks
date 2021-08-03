a51b13f64caaf6ff49cba9194714e89ba4841805
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-03 18:02:57 +0300

    Start implementing TryParseCommand

--- CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandParseOptions.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandParseOptions.cs
@@ -6,5 +6,11 @@ using System.Threading.Tasks;
 
 namespace CSharpPilot2.Commands
 {
-    internal record CommandParseOptions(string Prefix, string[] ParameterDelimiters);
+    internal record CommandParseOptions(string Prefix, string[] ParameterDelimiters)
+    {
+        public static bool TryParseCommand(string command, CommandParseOptions options, out CommandTemplate template)
+        {
+            
+        }
+    }
 }
