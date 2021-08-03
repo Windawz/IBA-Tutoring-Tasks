cb08af7ba1be32ea863c77f219410ddf2511d0f8
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-03 18:28:48 +0300

    Remove ParseCommand

--- CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandTemplate.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandTemplate.cs
@@ -6,21 +6,5 @@ using System.Threading.Tasks;
 
 namespace CSharpPilot2.Commands
 {
-    internal record CommandTemplate(string Name, ParameterTemplate[] Parameters)
-    {
-        /// <exception cref="ArgumentException"></exception>
-        public static CommandTemplate ParseCommand(string command, CommandParseOptions options)
-        {
-            if (String.IsNullOrEmpty(command))
-            {
-                throw new ArgumentException($"Command string is empty", nameof(command));
-            }
-            if (!command.StartsWith(options.Prefix))
-            {
-                throw new ArgumentException($"Command string doesn't start with {options.Prefix}", nameof(command));
-            }
-
-
-        }
-    }
+    internal record CommandTemplate(string Name, ParameterTemplate[] Parameters);
 }
