d27a7b3d7c42215e240932de670cc2630b158f6e
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-05 02:22:00 +0300

    Write Commands.Arg

---
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/Arg.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPilot2
+{
+    internal class Arg
+    {
+        public Arg(string value) =>
+            Value = value;
+
+        public string Value { get; init; }
+
+        public bool TryAsInteger(out int output) =>
+            int.TryParse(Value, out output);
+        public bool TryAsDouble(out double output) =>
+            double.TryParse(Value, out output);
+
+        public int AsIntegerOrDefault() =>
+            TryAsInteger(out int output) ? output : default;
+        public double AsDoubleOrDefault() =>
+            TryAsDouble(out double output) ? output : default;
+    }
+}
