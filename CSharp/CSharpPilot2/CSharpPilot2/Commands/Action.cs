1a073d47fdacee44f12e0cfb47cd9131266293d5
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-04 13:07:58 +0300

    Declare Commands.Action

---
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/Action.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPilot2.Commands
+{
+    delegate void Action(Context context);
+}
