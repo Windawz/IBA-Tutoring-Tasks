57b975cb25e8270655728446c33b4ea0a65bbf94
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-05 15:02:18 +0300

    Add CommandList
    Add CommandList for better control over the type of command lists.

---
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPilot2.Commands
+{
+    internal class CommandList
+    {
+        public CommandList(Dictionary<string, ParsedCommand> commands) =>
+            Commands = commands;
+
+        public Dictionary<string, ParsedCommand> Commands { get; }
+    }
+}
