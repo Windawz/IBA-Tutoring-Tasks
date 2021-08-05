7cf5d02bda8eb91629b22b642b8c9380a5cae4a0
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-05 19:23:20 +0300

    Move functions from Rules to Program
    Move GetDefaultInputValidator() and GetDefaultWordValidator() from Rules to Program.

--- CSharp/CSharpPilot2/CSharpPilot2/Gameplay/Rules.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Gameplay/Rules.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-
-using CSharpPilot2.Input;
+﻿using CSharpPilot2.Input;
 
 namespace CSharpPilot2.Gameplay
 {
@@ -19,24 +16,5 @@ namespace CSharpPilot2.Gameplay
         public WordValidator WordValidator { get; }
         public InputValidator InputValidator { get; }
         public InputForgiver InputForgiver { get; }
-
-        public static WordValidator GetDefaultWordValidator(RulesProperties properties) =>
-            (cur, prev) =>
-            {
-                if (String.Equals(cur.Text, prev.Text, StringComparison.InvariantCultureIgnoreCase)
-                    || (cur.Seconds > properties.MaxWordSeconds)
-                )
-                {
-                    return false;
-                }
-
-                System.Collections.Generic.IEnumerable<(char Char, int Count)>? curCounts = cur.Text.ToLowerInvariant().CharacterCounts();
-                System.Collections.Generic.IEnumerable<(char Char, int Count)>? prevCounts = prev.Text.ToLowerInvariant().CharacterCounts();
-
-                return curCounts.SequenceEqual(prevCounts);
-            };
-        public static InputValidator GetDefaultInputValidator(RulesProperties properties) =>
-            (inputInfo) => inputInfo.Text.Length <= properties.MaxWordTextLength
-                           && inputInfo.Text.Length >= properties.MinWordTextLength;
     }
 }
--- CSharp/CSharpPilot2/CSharpPilot2/Program.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Timers;
+using System.Linq;
 
 using CSharpPilot2.Gameplay;
 using CSharpPilot2.Input;
@@ -34,8 +35,26 @@ namespace CSharpPilot2
         {
             RulesProperties properties = new(2, 10.0, 8, 30);
 
-            return new Rules(properties, Rules.GetDefaultWordValidator(properties), Rules.GetDefaultInputValidator(properties),
+            return new Rules(properties, GetDefaultWordValidator(properties), GetDefaultInputValidator(properties),
                 inputInfo => false);
         }
+        private static WordValidator GetDefaultWordValidator(RulesProperties properties) =>
+            (cur, prev) =>
+            {
+                if (String.Equals(cur.Text, prev.Text, StringComparison.InvariantCultureIgnoreCase)
+                    || (cur.Seconds > properties.MaxWordSeconds)
+                )
+                {
+                    return false;
+                }
+
+                System.Collections.Generic.IEnumerable<(char Char, int Count)>? curCounts = cur.Text.ToLowerInvariant().CharacterCounts();
+                System.Collections.Generic.IEnumerable<(char Char, int Count)>? prevCounts = prev.Text.ToLowerInvariant().CharacterCounts();
+
+                return curCounts.SequenceEqual(prevCounts);
+            };
+        private static InputValidator GetDefaultInputValidator(RulesProperties properties) =>
+            (inputInfo) => inputInfo.Text.Length <= properties.MaxWordTextLength
+                           && inputInfo.Text.Length >= properties.MinWordTextLength;
     }
 }
