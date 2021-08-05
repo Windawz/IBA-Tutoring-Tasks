0e2fc32e2d88a78c0e81acc5be1548c80f80d1e0
Windawz <46968683+Windawz@users.noreply.github.com>
2021-08-05 19:02:31 +0300

    Fix formatting of CommandContext's constructor

--- CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandContext.cs
+++ CSharp/CSharpPilot2/CSharpPilot2/Commands/CommandContext.cs
@@ -10,8 +10,5 @@ using CSharpPilot2.Locales;
 namespace CSharpPilot2.Commands
 {
     // I hate to couple it with the Gameplay classes but I don't want to take another long detour.
-    internal record CommandContext(
-        Locale Locale,
-        State State
-    );
+    internal record CommandContext(Locale Locale, State State);
 }
