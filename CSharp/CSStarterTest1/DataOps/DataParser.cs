706b4cc3d971ac6892ef41ff6dc299d9cb40a904
Windawz <46968683+Windawz@users.noreply.github.com>
2021-09-25 19:25:09 +0300

    Work on `DataParser`:
    Make `DataParser` throw on failing to parse a line;
    Add documentation

--- CSharp/CSStarterTest1/DataOps/DataParser.cs
+++ CSharp/CSStarterTest1/DataOps/DataParser.cs
@@ -1,38 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace CSStarterTest1.DataOps
 {
+    /// <summary>
+    /// Parses text in .csv format into <see cref="Data"/>.
+    /// </summary>
     public sealed class DataParser
     {
         private static readonly int _dataFieldCount = Enum.GetValues(typeof(DataField)).Length;
 
         private readonly CSVParser _parser = new();
 
+        /// <summary>
+        /// Parses a string of representing a .csv file into an array of <see cref="Data"/> objects.
+        /// </summary>
+        /// <param name="data">The .csv file string.</param>
+        /// <returns>
+        /// An array of parsed <see cref="Data"/> objects.
+        /// </returns>
+        /// <exception cref="FormatException">Thrown if parsing a line fails.</exception>
         public Data[] Parse(string data)
         {
             string[][] table = _parser.Parse(data);
             List<Data> dataList = new();
             foreach (string[] line in table)
             {
-                Data? parsed = TryParseLine(line);
-                if (parsed is null)
-                {
-                    continue;
-                }
+                Data parsed = ParseLine(line);
                 dataList.Add(parsed);
             }
             return dataList.ToArray();
         }
-        private Data? TryParseLine(string[] line)
+
+        /// <summary>
+        /// Parses an array of tokens into a <see cref="Data"/> object.
+        /// </summary>
+        /// <param name="line">The array of tokens to parse.</param>
+        /// <returns>
+        /// The resulting <see cref="Data"/> object..
+        /// </returns>
+        /// <exception cref="FormatException">Thrown if parsing fails.</exception>
+        private static Data ParseLine(string[] line)
+        {
+            Data? data = TryParseLine(line);
+            if (data is null)
+            {
+                throw new FormatException($"Failed to parse line: \"{line.Aggregate((s1, s2) => s1 + s2)}\"");
+            }
+            return data;
+        }
+        /// <summary>
+        /// Parses an array of tokens into a <see cref="Data"/> object.
+        /// </summary>
+        /// <param name="line">The array of tokens to parse.</param>
+        /// <returns>
+        /// The resulting <see cref="Data"/> object, or null on failure.
+        /// </returns>
+        private static Data? TryParseLine(string[] line)
         {
             if (line.Length != _dataFieldCount)
             {
                 return null;
             }
 
-            if (!DateTime.TryParse(line[(int)DataField.Date], CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+            if (!DateTime.TryParse(line[(int)DataField.Date], CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
             {
                 return null;
             }
@@ -46,6 +79,9 @@ namespace CSStarterTest1.DataOps
                 line[(int)DataField.CountryName]
             );
         }
+        /// <summary>
+        /// Indices corresponding to the fields of a <see cref="Data"/> object.
+        /// </summary>
         private enum DataField
         {
             Date = 0,
