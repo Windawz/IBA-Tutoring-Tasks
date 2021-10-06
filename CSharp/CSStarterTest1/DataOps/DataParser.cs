f46af61a8a3e9d1a2f8fce5fa306e551d19eebfc
Windawz <46968683+Windawz@users.noreply.github.com>
2021-10-06 23:44:03 +0300

    Make `DataParser` use `FormatSettings.MainDateTimeFormatString`

--- CSharp/CSStarterTest1/DataOps/DataParser.cs
+++ CSharp/CSStarterTest1/DataOps/DataParser.cs
@@ -65,7 +65,7 @@ namespace CSStarterTest1.DataOps
                 return null;
             }
 
-            if (!DateTime.TryParseExact(line[(int)DataField.Date], "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
+            if (!DateTime.TryParseExact(line[(int)DataField.Date], FormatSettings.MainDateTimeFormatString, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date))
             {
                 return null;
             }
