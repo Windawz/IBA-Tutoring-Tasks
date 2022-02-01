0cf0d55fbb5d74daef61b5891df36ccfc92d9849
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-01 13:06:51 +0300

    Add IImporter interface for all importers

---
+++ CSharp/CSStarterTest1/DataOps/New/IImporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSStarterTest1.DataOps.New
+{
+    public interface IImporter
+    {
+        object? Import(string path);
+    }
+}
