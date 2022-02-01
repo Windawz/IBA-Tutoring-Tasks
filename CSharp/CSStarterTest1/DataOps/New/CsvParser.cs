9371990a52cc81ddf668d0af0dd13f99c1867caf
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-01 14:18:05 +0300

    Implement CsvParser

---
+++ CSharp/CSStarterTest1/DataOps/New/CsvParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSStarterTest1.DataOps.New
+{
+    /// <summary>
+    /// Parses .csv strings into data records
+    /// </summary>
+    internal class CsvParser
+    {
+        private static readonly int DataFieldCount = typeof(Data).GetProperties().Length;
+
+        public Data[] Parse(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                return Array.Empty<Data>();
+            }
+            string[] lines = text.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var datas = new List<Data>(lines.Length);
+            foreach (string line in lines)
+            {
+                Data? data = ParseLine(line);
+                if (data is not null)
+                {
+                    datas.Add(data);
+                }
+            }
+            return datas.ToArray();
+        }
+        private Data? ParseLine(string line)
+        {
+            string[] tokens = line.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if (tokens.Length < DataFieldCount)
+            {
+                return null;
+            }
+
+            Data data;
+            try
+            {
+                data = new Data(
+                    int.Parse(tokens[0]),
+                    DateTime.Parse(tokens[1]),
+                    tokens[2],
+                    tokens[3],
+                    tokens[4],
+                    tokens[5],
+                    tokens[6]
+                );
+            }
+            catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+            {
+                return null;
+            }
+
+            return data;
+        }
+    }
+}
