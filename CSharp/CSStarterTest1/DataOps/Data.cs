d35bd14dc893d60ed72b2024a9045c3993dc9263
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-06 16:14:14 +0300

    Make the Data type compatible with EF Core
    EF Core seems to rely on referential equality, and records provide value equality. Had to rewrite this as a class.

--- CSharp/CSStarterTest1/DataOps/Data.cs
+++ CSharp/CSStarterTest1/DataOps/Data.cs
@@ -5,8 +5,27 @@ namespace CSStarterTest1.DataOps
     /// <summary>
     /// Represents the most complete piece of data.
     /// </summary>
-    public record Data(int Id, DateTime Date, string FirstName, string LastName, string SurName, string City, string Country)
+    public class Data
     {
         public static readonly int FieldCount = typeof(Data).GetProperties().Length;
+
+        public Data(int id, DateTime date, string firstName, string lastName, string surName, string city, string country)
+        {
+            Id = id;
+            Date = date;
+            FirstName = firstName;
+            LastName = lastName;
+            SurName = surName;
+            City = city;
+            Country = country;
+        }
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
     }
 }
