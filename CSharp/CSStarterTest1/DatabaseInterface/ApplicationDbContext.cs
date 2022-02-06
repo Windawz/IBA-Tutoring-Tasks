60f0cb1ad0585fed4728a2992b4616681c26af8f
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-06 16:52:40 +0300

    Write custom DbContext

---
+++ CSharp/CSStarterTest1/DatabaseInterface/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CSStarterTest1.DataOps;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace DatabaseInterface {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(string connectionString)
+        {
+            try
+            {
+                // Validate connection string
+                _ = new SqlConnectionStringBuilder(connectionString);
+            }
+            catch (Exception ex) when 
+            (
+                ex is System.Collections.Generic.KeyNotFoundException
+                or FormatException
+                or ArgumentException
+            )
+            {
+                throw new ArgumentException
+                (
+                    "Invalid connection string. See inner exception",
+                    nameof(connectionString),
+                    ex
+                );
+            }
+
+            ConnectionString = connectionString;
+        }
+
+        public string ConnectionString { get; }
+        public DbSet<Data> Datas { get; set; } = null!; // To avoid nullable reference type warnings.
+                                                        // Won't actually be null.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
