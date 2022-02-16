4afc79e248a758f54d7c864d46d2b09a2e8660f4
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-16 13:14:42 +0300

    Write GetWriter and SetWriter for ConsoleOutput

---
+++ CSharp/CSStarterTest1/Tester/ConsoleOutputExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace CSStarterTest1.Tester
+{
+    internal static class ConsoleOutputExtensions
+    {
+        /// <summary>
+        /// Returns the corresponding standard output stream of the console.
+        /// </summary>
+        public static TextWriter GetWriter(this ConsoleOutput consoleOutput) => consoleOutput switch
+        {
+            ConsoleOutput.Out => Console.Out,
+            ConsoleOutput.Error => Console.Error,
+            _ => throw new ArgumentOutOfRangeException(nameof(consoleOutput)),
+        };
+        public static void SetWriter(this ConsoleOutput consoleOutput, TextWriter newWriter)
+        {
+            switch (consoleOutput)
+            {
+                case ConsoleOutput.Out:
+                    Console.SetOut(newWriter);
+                    break;
+                case ConsoleOutput.Error:
+                    Console.SetError(newWriter);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(consoleOutput));
+            }
+        }
+    }
+}
