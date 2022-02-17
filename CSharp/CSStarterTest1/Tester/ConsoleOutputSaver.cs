417d7221e7d54ce725599bafda8e0f22350538ee
Windawz <46968683+Windawz@users.noreply.github.com>
2022-02-17 08:58:44 +0300

    Write ConsoleOutputSaver

---
+++ CSharp/CSStarterTest1/Tester/ConsoleOutputSaver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CSStarterTest1.Tester
+{
+    /// <summary>
+    /// Takes the specified console output, copies the writer and keeps it.
+    /// When disposed, assigns the writer back.
+    /// </summary>
+    internal class ConsoleOutputSaver : IDisposable
+    {
+        /// <param name="output">Console output to save.</param>
+        public ConsoleOutputSaver(ConsoleOutput output)
+        {
+            _output = output;
+            _old = output.GetWriter();
+        }
+        
+        private readonly TextWriter _old;
+        private readonly ConsoleOutput _output;
+        private bool _disposed;
+
+        public void Restore() => Dispose();
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _output.SetWriter(_old);
+                _disposed = true;
+            }
+        }
+    }
+}
